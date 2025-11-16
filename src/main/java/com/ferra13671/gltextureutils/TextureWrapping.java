60088f11e1fe224032d93e2b2fe0f90d4384bcec
Ferra13671 <ferra13671@gmail.com>
2025-11-16 18:41:01 +0300

    Add TextureWrapping.MIRROR_REPEAT

--- src/main/java/com/ferra13671/gltextureutils/TextureWrapping.java
+++ src/main/java/com/ferra13671/gltextureutils/TextureWrapping.java
@@ -2,6 +2,7 @@ package com.ferra13671.gltextureutils;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL14;
 
 /**
  * OpenGL behavior mode when getting a pixel color outside the range [0, 1].
@@ -10,13 +11,17 @@ import org.lwjgl.opengl.GL12;
  */
 public enum TextureWrapping {
     /**
-     * A mode in which values outside the bounds are compressed to the edges of the texture.
+     * A mode in which values outside the boundaries will return the color of the pixel closest to the boundary.
      */
     DEFAULT(GL12.GL_CLAMP_TO_EDGE),
     /**
      * The mode in which the texture will be repeated beyond the boundaries.
      */
-    REPEAT(GL11.GL_REPEAT);
+    REPEAT(GL11.GL_REPEAT),
+    /**
+     * Works the same as REPEAT, except that each repetition of the texture mirrors it.
+     */
+    MIRROR_REPEAT(GL14.GL_MIRRORED_REPEAT);
 
     public final int id;
 
