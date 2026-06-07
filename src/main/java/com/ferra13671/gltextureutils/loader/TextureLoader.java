e22f5bb90dcebd7d9babfc1e83188906ea1bbd3a
Ferra13671 <ferra13671@gmail.com>
2026-06-07 20:37:46 +0300

    Make TextureLoader an interface

--- src/main/java/com/ferra13671/gltextureutils/loader/TextureLoader.java
+++ src/main/java/com/ferra13671/gltextureutils/loader/TextureLoader.java
@@ -22,63 +22,47 @@ import java.util.concurrent.atomic.AtomicReference;
  *
  * @see GLTexture
  */
-public abstract class TextureLoader<T> {
-    public static final TextureLoader<InputStream> INPUT_STREAM = new TextureLoader<>() {
-        @Override
-        public GLTextureInfo load(InputStream path, ColorMode colorMode) throws Exception {
-            ByteBuffer buffer = Utils.readStream(path);
-            buffer.rewind();
+public interface TextureLoader<T> {
+    TextureLoader<InputStream> INPUT_STREAM = (path, colorMode) -> {
+        ByteBuffer buffer = Utils.readStream(path);
+        buffer.rewind();
 
-            AtomicReference<GLTextureInfo> glTextureInfo = new AtomicReference<>(null);
-            Utils.tryGenerate(buffer, path, memoryStack -> {
-                IntBuffer xBuffer = memoryStack.mallocInt(1);
-                IntBuffer yBuffer = memoryStack.mallocInt(1);
-                IntBuffer channelsBuffer = memoryStack.mallocInt(1);
-                ByteBuffer byteBuffer = STBImage.stbi_load_from_memory(buffer, xBuffer, yBuffer, channelsBuffer, colorMode == ColorMode.RGBA ? 4 : 3);
+        AtomicReference<GLTextureInfo> glTextureInfo = new AtomicReference<>(null);
+        Utils.tryGenerate(buffer, path, memoryStack -> {
+            IntBuffer xBuffer = memoryStack.mallocInt(1);
+            IntBuffer yBuffer = memoryStack.mallocInt(1);
+            IntBuffer channelsBuffer = memoryStack.mallocInt(1);
+            ByteBuffer byteBuffer = STBImage.stbi_load_from_memory(buffer, xBuffer, yBuffer, channelsBuffer, colorMode == ColorMode.RGBA ? 4 : 3);
 
-                if (byteBuffer != null)
-                    glTextureInfo.set(new GLTextureInfo(byteBuffer, xBuffer.get(0), yBuffer.get(0), true));
-            });
-            return glTextureInfo.get();
-        }
+            if (byteBuffer != null)
+                glTextureInfo.set(new GLTextureInfo(byteBuffer, xBuffer.get(0), yBuffer.get(0), true));
+        });
+        return glTextureInfo.get();
     };
-    public static final TextureLoader<FileEntry> FILE_ENTRY = new TextureLoader<>() {
-        @Override
-        public GLTextureInfo load(FileEntry path, ColorMode colorMode) throws Exception {
-            return INPUT_STREAM.load(path.pathMode().streamCreateFunction.apply(path.path()), colorMode);
-        }
-    };
-    public static final TextureLoader<URL> URL = new TextureLoader<>() {
-        @Override
-        public GLTextureInfo load(URL path, ColorMode colorMode) throws Exception {
-            return INPUT_STREAM.load(path.openStream(), colorMode);
-        }
-    };
-    public static final TextureLoader<BufferedImage> BUFFERED_IMAGE = new TextureLoader<>() {
-        @Override
-        public GLTextureInfo load(BufferedImage path, ColorMode colorMode) {
-            GLTextureInfo glTextureInfo;
+    TextureLoader<FileEntry> FILE_ENTRY = (path, colorMode) -> INPUT_STREAM.load(path.pathMode().streamCreateFunction.apply(path.path()), colorMode);
+    TextureLoader<URL> URL = (path, colorMode) -> INPUT_STREAM.load(path.openStream(), colorMode);
+    TextureLoader<BufferedImage> BUFFERED_IMAGE = (path, colorMode) -> {
+        GLTextureInfo glTextureInfo;
 
-            int[] pixels = new int[path.getWidth() * path.getHeight()];
-            path.getRGB(0, 0, path.getWidth(), path.getHeight(), pixels, 0, path.getWidth());
+        int[] pixels = new int[path.getWidth() * path.getHeight()];
+        path.getRGB(0, 0, path.getWidth(), path.getHeight(), pixels, 0, path.getWidth());
 
-            ByteBuffer byteBuffer = MemoryUtil.memAlloc((path.getWidth() * path.getHeight() * 4));
+        ByteBuffer byteBuffer = MemoryUtil.memAlloc((path.getWidth() * path.getHeight() * 4));
 
-            for (int y = 0; y < path.getHeight(); y++) {
-                for (int x = 0; x < path.getWidth(); x++) {
-                    int pixel = pixels[y * path.getWidth() + x];
-                    byteBuffer.put(Utils.getRed(pixel));
-                    byteBuffer.put(Utils.getGreen(pixel));
-                    byteBuffer.put(Utils.getBlue(pixel));
-                    byteBuffer.put(Utils.getAlpha(pixel));
-                }
+        for (int y = 0; y < path.getHeight(); y++) {
+            for (int x = 0; x < path.getWidth(); x++) {
+                int pixel = pixels[y * path.getWidth() + x];
+                byteBuffer.put(Utils.getRed(pixel));
+                byteBuffer.put(Utils.getGreen(pixel));
+                byteBuffer.put(Utils.getBlue(pixel));
+                byteBuffer.put(Utils.getAlpha(pixel));
             }
-            byteBuffer.flip();
-
-            glTextureInfo = new GLTextureInfo(byteBuffer, path.getWidth(), path.getHeight(), false);
-
-            return glTextureInfo;
         }
+        byteBuffer.flip();
+
+        glTextureInfo = new GLTextureInfo(byteBuffer, path.getWidth(), path.getHeight(), false);
+
+        return glTextureInfo;
     };
 
     /**
@@ -91,7 +75,7 @@ public abstract class TextureLoader<T> {
      *
      * @see GLTextureInfo
      */
-    public abstract GLTextureInfo load(T path, ColorMode colorMode) throws Exception;
+    GLTextureInfo load(T path, ColorMode colorMode) throws Exception;
 
     /**
      * Creates a new texture builder that will use this texture loader.
@@ -100,7 +84,7 @@ public abstract class TextureLoader<T> {
      *
      * @see GLTextureBuilder
      */
-    public GLTextureBuilder<T> createTextureBuilder() {
+    default GLTextureBuilder<T> createTextureBuilder() {
         return new GLTextureBuilder<>(this);
     }
 }
